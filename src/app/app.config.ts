97b15d0d9ec3370adc50a4c38acd0fd124a1f1a5
Mario Beltran <mbeltran@betacode.dev>
2025-12-23 01:12:44 -0600

    🔧 add initial config on project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,29 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import {
+  ApplicationConfig,
+  isDevMode,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-
+import { provideEffects } from '@ngrx/effects';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(withFetch()),
+    provideStore(),
+    provideEffects(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+]
 };
--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,30 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'products',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'products/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'cart',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'checkout',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'order-success',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
